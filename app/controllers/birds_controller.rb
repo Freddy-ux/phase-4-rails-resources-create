928012fa6f40e2c8e71fc02ec2aa0e1ea320e87c
Freddy-ux <alfredmuleri569@gmail.com>
2023-06-20 23:15:20 +0300

    Done with Assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,19 +1,9 @@
 class BirdsController < ApplicationController
 
-  # GET /birds
-  def index
-    birds = Bird.all
-    render json: birds
+  # POST /birds
+  def create
+    byebug
   end
 
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
-  end
-
+  # etc
 end
